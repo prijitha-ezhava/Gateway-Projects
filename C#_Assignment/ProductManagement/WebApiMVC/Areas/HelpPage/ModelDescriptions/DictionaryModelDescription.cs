5ac31cb325bf1612a4a70483da7d2c24c25af597
Prijitha Ezhava <74618901+prijitha-ezhava@users.noreply.github.com>
2021-04-07 15:05:00 +0530

    removed

--- C#_Assignment/ProductManagement/WebApiMVC/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WebApiMVC.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- C#_Assignment/ProductManagement/WebApiMVC/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WebApiMVC.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
